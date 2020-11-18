b43b2a763421a9d5abd6c12d9a115c95b95b0608
aarni <aarni.alasaarela@gmail.com>
2020-11-18 17:58:07 +0200

    remove marshmallow annotations and add model for post_comment

--- setup.py
+++ setup.py
@@ -31,9 +31,7 @@ setup(
         # long as they have a version of pyfoobar equal to or greater than 1.x
         # and less than 2.x installed.
         'flask>=1.0.2',
-        'marshmallow>=2.15.3,<=3.6',
-        ('git+https://www.github.com/hilearn/marshmallow-annotations.git@a7a2dc96932430369bd'
-         'ef36555082df990ed9bef#egg=marshmallow-annotations')
+        'marshmallow>=2.15.3,<=3.6'
     ],
     python_requires=">=3.6",
     package_data={'amundsen_common': ['py.typed']},
