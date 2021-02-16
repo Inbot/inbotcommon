b5d98d4badb46de8b167f486ac9af75c2aa069cc
aarni <aarni.alasaarela@gmail.com>
2021-02-16 12:44:58 +0200

    update maintainer name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Inbot/amundsencommon',
-    maintainer='Aarni',
+    maintainer='Aarni Alasaarela',
     maintainer_email='aarni.alasaarela@gmail.com',
     packages=find_packages(exclude=['tests*']),
     dependency_links=[],
